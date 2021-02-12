7e74a05bff11b92bf715a1a5d33c2f478c00ab00
Rashmi <dev.rvenkatesh@gmail.com>
2021-02-11 21:28:38 -0500

    first commit

---
+++ greetme.sh
@@ -0,0 +1,38 @@
+#----------------------------
+
+#Author:Rashmi Venkatesh 
+#Date: 02/11/2021
+
+
+#Description: The purpose of this script to try using cli for doing some of the tasks commented out below. 
+#
+#
+
+#---------------------------
+# Version: 1.0
+#
+#Special Instructions:
+
+#Declared Variables:
+
+#---------------------------
+
+
+#Script Body 
+
+
+
+echo "HellO $USER" 
+echo $(date)  
+echo $(cal -j) 
+echo $(hostname)
+uname -mor
+ls ..
+ps -aux 
+printf $TERM
+printf $PATH
+printf $HOME
+df -h ${pwd} 
+id -g
+printf "Please couldn't you loan me \$50.00?"
+echo "Good bye" $(date +%H) 
